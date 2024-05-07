fdbccad5efb1d323713763da7bc4de400278e1ca
sphynixstorez <sphynixstore@gmail.com>
2024-05-07 14:47:19 +0700

    Update owner-dan-menu.js

--- owner-dan-menu.js
+++ owner-dan-menu.js
@@ -1,24 +1,20 @@
 const fs = require('fs')
 
-global.namabot = "KURUMI-BOT"
-global.namaowner = "Lycoris-Recoil"
+global.namabot = "Bot Store"
+global.namaowner = "Zeeoneofc"
 global.footer_text = "© " + namabot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['6285817287205','6285778324998']
+global.owner = ['628577776666','628577775555']
+global.pairingNumber = ""
 global.sessionName = 'session'
 global.prefa = ['-_-']
-global.caption_pay = `[ PAYMENT SPHYNIXSTORE ]
+global.caption_pay = `Qris All Pay
+Ovo
+Dana
+Gopay
 
-Qris : Scan barcode di atas
-
-Dana : 085817287205
-A/n : Rahmad
-
-Shopeepay : 089524882317
-A/n : sphynixstore
-
-[ Silahkan melakukan pembayaran ]
+Mau ganti payment? ketik .setpay
 `
 //menu bot rapihin sendiri ya, belajar lah jadi anak mandiri.
 module.exports.helpMenu = (pushname) =>{
